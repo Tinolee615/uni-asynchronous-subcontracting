e89e49f104dfb8825e1c34257d6e711d623f7330
刘炳辰 <liubingchen@qtshe.com>
2024-03-06 09:30:45 +0800

    feat: 适配vue3

--- scripts/patch-package-init.js
+++ scripts/patch-package-init.js
@@ -1,9 +1,17 @@
 const execSync = require('child_process').execSync;
 const { INIT_CWD, PWD } = process.env;
+const packageConfig = require('../package.json')
+try {
+  const vue3verify = /^\^3.*.*/.test(packageConfig.dependencies.vue)
+  const patchDir = vue3verify ? '--patch-dir patches-vue3' : '--patch-dir patches'
 
-if (!INIT_CWD || INIT_CWD === PWD || INIT_CWD.toString().replace(/\\/g, () => '/').indexOf(PWD) === 0) {
-  const command = ['npx', ...process.argv.slice(2, process.argv.length)].join(' ');
-  execSync(command, { stdio: 'inherit' });
+  if (!INIT_CWD || INIT_CWD === PWD || INIT_CWD.toString().replace(/\\/g, () => '/').indexOf(PWD) === 0) {
+    const command = ['npx', ...process.argv.slice(2, process.argv.length), patchDir].join(' ');
+    console.log('[command]', command);
+    execSync(command, { stdio: 'inherit' });
+  }
+  
+  process.exit(0);
+} catch (error) {
+  console.error(error);
 }
-
-process.exit(0);
